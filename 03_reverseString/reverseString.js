79c19a2c5624caadbbd4a91ab14f70af5ab82701
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-22 18:20:27 -0500

    03 tests passing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+// const reverseString = function (string) {
+//   let reverseStrArr = [];
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reverseStrArr.push(string[i]);
+//   }
 
-};
+//   return reverseStrArr.join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
