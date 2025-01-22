274fbe478ed4ec31a49ddbc51780ebf124c27b3c
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-22 17:48:20 -0500

    02 complete: tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  if (num === 0) return "";
+  if (num === 1) return string;
 
+  let stringResult = string;
+  for (let i = 1; i < num; i++) {
+    stringResult += string;
+  }
+
+  return stringResult;
 };
 
 // Do not edit below this line
