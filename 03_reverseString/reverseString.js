8d2a47a10e463e1d19e21459351887f8c5ba68c1
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-22 18:21:01 -0500

    refactor reverseString f

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,5 +7,23 @@
 //   return reverseStrArr.join("");
 // };
 
+const reverseString = function (string) {
+  let strArr = string.split("");
+  let left = 0;
+  let right = strArr.length - 1;
+
+  while (left < right) {
+    // Swap characters
+    let temp = strArr[left];
+    strArr[left] = strArr[right];
+    strArr[right] = temp;
+
+    left++;
+    right--;
+  }
+
+  return strArr.join("");
+};
+
 // Do not edit below this line
 module.exports = reverseString;
