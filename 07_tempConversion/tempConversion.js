7da567fcdc609636c64097e44105c393cd3e5b33
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-23 17:38:56 -0500

    07 tests passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  return Number(((5 / 9) * (num - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  return Number((1.8 * num + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
