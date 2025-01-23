667926cb1d4abf7eced8ef475734a30ffcaa8453
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-22 19:30:02 -0500

    04 tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let result = [];
+
+  for (let item of arr) {
+    if (!args.includes(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
 };
+
+// const removeFromArray = (arr, ...args) => {
+//   return arr.filter((item) => !args.includes(item));
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
