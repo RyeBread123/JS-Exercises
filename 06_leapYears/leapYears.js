f827197bde3c47c4da954636231b8b98cbfa1ce6
RyeBread123 <53073301+RyeBread123@users.noreply.github.com>
2025-01-23 17:28:10 -0500

    06 tests passing

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // return false if the year is not divisble by 4
+  if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) return false;
 
+  return true;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
